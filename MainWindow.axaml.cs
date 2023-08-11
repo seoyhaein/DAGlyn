cd75686133a8fdb6c4bb4c73b6ce49365709813c
HeaInSeo <jwsnsl@knou.ac.kr>
2023-08-11 16:28:53 +0900

    렌더링이 안되는 문제가 있음.

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,10 @@
+using Avalonia;
 using Avalonia.Controls;
+
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using Avalonia.Collections;
+using Avalonia.Media;
 
 namespace DAGlyn
 {
@@ -7,6 +13,98 @@ namespace DAGlyn
         public MainWindow()
         {
             InitializeComponent();
+            //CreatePendingConnection();
+            //Tester.ItemsSource = Setup();
         }
+
+        /*private void CreatePendingConnection()
+        {
+            Point sourceAnchor = new Point(10, 10);
+            Point targetAnchor = new Point(100, 100);
+            AvaloniaList<double>? strokeDashArray = null;
+            IBrush? stroke = Brushes.Black;
+            double strokeThickness = 2;
+            ConnectionDirection direction = ConnectionDirection.Forward;
+            IBrush? fill = Brushes.Black;
+
+            PendingConnection pendingConnection = new PendingConnection(sourceAnchor, targetAnchor, strokeDashArray, stroke, strokeThickness, direction, fill);
+
+            // Grid에 추가
+            var grid = this.FindControl<Grid>("grid");
+            grid.Children.Add(pendingConnection);
+
+            // 생성한 PendingConnection 인스턴스를 컨트롤 트리에 추가하거나 다른 작업을 수행합니다.
+            // 예시: this.Content = pendingConnection;
+        }*/
+    }
+
+    public class ConnectionsViewModel 
+    {
+        public ObservableCollection<ConnectionViewModel> Connections { get; } = new ObservableCollection<ConnectionViewModel>();
+
+        public ConnectionsViewModel()
+        {
+            Connections.Add(new ConnectionViewModel
+            {
+                Source = new ConnectorViewModel
+                {
+                    Title = "Start",
+                    Anchor = new Point(10, 10)
+                },
+
+                Target = new ConnectorViewModel
+                {
+                    Title = "End",
+                    Anchor = new Point(100, 100)
+                }
+            });
+        }
+        
+        public static ObservableCollection<ConnectionViewModel> Setup()
+        {
+            ObservableCollection<ConnectionViewModel> Connections = new ObservableCollection<ConnectionViewModel>
+            {
+                new ConnectionViewModel
+                {
+                    Source = new ConnectorViewModel
+                    {
+                        Title = "Start",
+                        Anchor = new Point(10, 10)
+                    },
+
+                    Target = new ConnectorViewModel
+                    {
+                        Title = "End",
+                        Anchor = new Point(100, 100)
+                    }
+                }
+            };
+
+            return Connections;
+        }
+    }
+
+    public class ConnectionViewModel
+    {
+        public ConnectorViewModel? Source { get; set; }
+        public ConnectorViewModel? Target { get; set; }
+    }
+
+    public class ConnectorViewModel : INotifyPropertyChanged
+    {
+        private Point _anchor;
+        public Point Anchor
+        {
+            set
+            {
+                _anchor = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Anchor)));
+            }
+            get => _anchor;
+        }
+
+        public string? Title { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
