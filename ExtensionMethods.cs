a8ce9f050bddfe1e9f0eea00d8c03102bbd27ce5
HeaInSeo <jwsnsl@knou.ac.kr>
2023-08-24 23:06:41 +0900

    업데이트

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia.Reactive;
 
 using System;
+using System.Diagnostics;
 
 // AvaloniaEdit Utils 참고.
 // 완벽하게 이해하진 못했음.
@@ -8,6 +9,17 @@ namespace DAGlyn
 {
     public static class ExtensionMethods
     {
+        [Conditional("DEBUG")]
+        public static void Log(bool condition, string format, params object[] args)
+        {
+            if (condition)
+            {
+                string output = DateTime.Now.ToString("hh:MM:ss") + ": " + string.Format(format, args); //+ Environment.NewLine + Environment.StackTrace;
+                //Console.WriteLine(output);
+                Debug.WriteLine(output);
+            }
+        }
+
         public static IDisposable Subscribe<T>(this IObservable<T> observable, Action<T> action)
         {
             return observable.Subscribe(new AnonymousObserver<T>(action));
